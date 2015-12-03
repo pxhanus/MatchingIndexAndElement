9660295432eccf63aa4dd7cac685965d3c2517ff
pxhanus <pxhanus@gmail.com>
2015-12-03 15:09:15 -0600

    Initial commit

---
+++ MatchingIndexAndElement/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lstNumbers = New System.Windows.Forms.ListBox()
+        Me.lblMatch = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(175, 14)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(80, 35)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate/ Match"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lstNumbers
+        '
+        Me.lstNumbers.FormattingEnabled = True
+        Me.lstNumbers.Location = New System.Drawing.Point(12, 14)
+        Me.lstNumbers.Name = "lstNumbers"
+        Me.lstNumbers.Size = New System.Drawing.Size(157, 160)
+        Me.lstNumbers.TabIndex = 1
+        '
+        'lblMatch
+        '
+        Me.lblMatch.Location = New System.Drawing.Point(175, 68)
+        Me.lblMatch.Name = "lblMatch"
+        Me.lblMatch.Size = New System.Drawing.Size(80, 43)
+        Me.lblMatch.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(267, 188)
+        Me.Controls.Add(Me.lblMatch)
+        Me.Controls.Add(Me.lstNumbers)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Matching Index And Element"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents lstNumbers As System.Windows.Forms.ListBox
+    Friend WithEvents lblMatch As System.Windows.Forms.Label
+
+End Class
---
+++ MatchingIndexAndElement/Form1.vb
@@ -0,0 +1,54 @@
+﻿'Patricia Hanus
+'151203
+'Matching Index And Element
+'_____________________________________________________________________________
+Public Class Form1
+    Dim numbers(9) As Integer
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Me.lstNumbers.Items.Clear()
+
+        For index As Integer = 0 To 9
+            numbers(index) = RndInt(0, (numbers.Length - 1))
+        Next
+
+        Call DisplayArray(Me.lstNumbers, numbers)
+        Call MatchNums(numbers, Me.lblMatch)
+
+    End Sub
+    '********************************************************************************************
+    'RndInt
+    'Returns a random integer between max and min
+    '
+    'post:  A number is returned
+    '********************************************************************************************
+    Function RndInt(ByVal min As Integer, ByVal max As Integer) As Integer
+        Randomize()
+        Return Int((max - min + 1) * Rnd() + min)
+    End Function
+    '********************************************************************************************
+    'DisplayArray
+    'Displays the contents of the passed array in the passed list box values index
+    '
+    'post:  Array results displayed in list box 
+    '********************************************************************************************
+    Sub DisplayArray(ByRef lstbox As ListBox, ByRef array() As Integer)
+        For index As Integer = 0 To array.Length - 1
+            lstbox.Items.Add(index & vbTab & array(index))
+        Next
+    End Sub
+    '********************************************************************************************
+    'MatchNums
+    'Checks for matching index and element
+    '	
+    'post: Returns number of matching index/element pairs
+    '********************************************************************************************
+    Sub MatchNums(ByRef array() As Integer, ByRef label As Label)
+        Dim numMatch As Integer = 0
+        For i As Integer = 0 To array.Length - 1
+            If array(i) = i Then
+                numMatch += 1
+            End If
+        Next
+        label.Text = numMatch & " elements matched their index."
+    End Sub
+End Class
